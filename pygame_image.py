9480d84904fc063a713bfe2c3c0747c7a8b79199
c0b23069 <c0b230692f@edu.teu.ac.jp>
2024-09-24 17:02:55 +0900

    lastcommit

--- pygame_image.py
+++ pygame_image.py
@@ -10,23 +10,34 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kouka = pg.image.load("fig/3.png")
-    kouka = pg.transform.flip(kouka,True,False)
-    kouka_rect = kouka.get_rect()
+    bg_img2= pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")  # 練習２
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習２
+    kk_rct = kk_img.get_rect()  # 練習8-1：SurfaceからRectを抽出する
+    kk_rct.center = 300, 200  # 練習8-2：Rectを使った初期座標の設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()  # 練習8-3：キーの押下状態を取得
+        if key_lst[pg.K_UP]:  # 上矢印キーがTrueなら
+            kk_rct.move_ip((0, -1))  # こうかとんの縦座標を-1する
+        if key_lst[pg.K_DOWN]:  # 下矢印キーがTrueなら
+            kk_rct.move_ip((0, +1))  # こうかとんの縦座標を+1する
+        if key_lst[pg.K_LEFT]:  # 下矢印キーがTrueなら
+            kk_rct.move_ip((-1, 0))  # こうかとんの横座標を-1する
+        if key_lst[pg.K_RIGHT]:  # 下矢印キーがTrueなら
+            kk_rct.move_ip((+1, 0))  # こうかとんの横座標を+1する
 
-        x = -(tmr%1600)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img,[x+1600,0])
-        screen.blit(bg_img,[x+3200,0])
-        screen.blit(bg_img,[x+4800,0])
-        screen.blit(kouka,[300,200])
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
+        screen.blit(kk_img, kk_rct)  # 練習４ -> 練習8-5
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(400)  # 練習５
 
 
 if __name__ == "__main__":
